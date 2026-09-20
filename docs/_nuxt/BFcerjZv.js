a3076d33c00a2068448f6ef0dbaba63a1dc25389
danharr <danharr@users.noreply.github.com>
2026-09-20 09:09:18 +0100

    weekly update

---
+++ docs/_nuxt/BFcerjZv.js
@@ -0,0 +1,53 @@
+import{p as t,t as e}from"./D0kzHlpo.js";const a=`team_abbr,team_name,matches_played,wins,draws,losses,points
+MUN,Manchester United,1308,776,290,242,2618
+ARS,Arsenal,1309,723,316,270,2485
+LIV,Liverpool,1308,695,323,290,2408
+CHE,Chelsea,1309,683,324,302,2373
+TOT,Tottenham Hotspur,1309,561,311,437,1994
+MCI,Manchester City,1118,577,239,302,1970
+EVE,Everton,1309,465,369,475,1764
+NEW,Newcastle United,1191,455,299,437,1664
+AVL,Aston Villa,1195,431,329,435,1622
+WHU,West Ham United,1148,381,289,478,1432
+SOU,Southampton,962,282,254,426,1100
+BLA,Blackburn Rovers,696,262,184,250,970
+LEE,Leeds United,624,236,167,221,875
+FUL,Fulham,726,220,186,320,846
+LEI,Leicester City,688,224,174,290,846
+CRY,Crystal Palace,658,194,177,287,759
+SUN,Sunderland,650,168,172,310,676
+MID,Middlesbrough,574,165,169,240,664
+BOL,Bolton Wanderers,494,149,128,217,575
+WOL,Wolverhampton Wanderers,456,129,110,217,497
+WBA,West Bromwich Albion,494,117,139,238,490
+STO,Stoke City,380,116,109,155,457
+BHA,Brighton and Hove Albion,347,111,110,126,443
+NFO,Nottingham Forest,354,109,100,145,427
+BOU,Bournemouth,346,108,90,148,414
+COV,Coventry City,358,99,112,147,409
+NOR,Norwich City,392,99,105,188,402
+SHW,Sheffield Wednesday,316,101,89,126,392
+WIM,Wimbledon,316,99,94,123,391
+BUR,Burnley,380,92,95,193,371
+CHA,Charlton Athletic,304,93,82,129,361
+WIG,Wigan Athletic,304,85,76,143,331
+SWA,Swansea City,266,82,66,118,312
+QPR,Queens Park Rangers,278,81,65,132,308
+POR,Portsmouth,266,79,65,122,302
+BIR,Birmingham City,266,73,82,111,301
+WAT,Watford,304,73,66,165,285
+DER,Derby County,266,68,70,128,274
+BRE,Brentford,195,70,52,73,262
+IPS,Ipswich Town,245,63,63,119,252
+SHU,Sheffield United,236,56,57,123,225
+HUL,Hull City,195,43,50,102,179
+REA,Reading,114,32,23,59,119
+OLD,Oldham Athletic,84,22,23,39,89
+CAR,Cardiff City,76,17,13,46,64
+BRA,Bradford City,76,14,20,42,62
+HUD,Huddersfield Town,76,12,17,47,53
+BPL,Blackpool,38,10,9,19,39
+BAR,Barnsley,38,10,5,23,35
+SWI,Swindon Town,42,5,15,22,30
+LUT,Luton Town,38,6,8,24,26
+`,s=t(a,n=>({team_abbr:n.team_abbr,team_name:n.team_name,matches_played:e(n,"matches_played"),wins:e(n,"wins"),draws:e(n,"draws"),losses:e(n,"losses"),points:e(n,"points")}));export{s as a};
